0768aefe9a8a6f97789f63bd50a951c3070195ae
Eloizi <eloizisilva16@gmail.com>
2024-02-14 09:52:35 -0300

    alterações

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+listaDeTeclas[0].onclick = tocaSom;
 
 let contador = 0;
 
 //enquanto
 while (contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = function (){
+        tocaSom('#som_tecla_pom');
+    }
 
     contador++;
 
